ebb43dcf8a3bfdf90fc3b6d58a5c9f3f2bef6112
Fernando <fernandostarlin1012@gmail.com>
2023-02-16 18:37:39 -0400

    Now we have X's and O's

--- src/index.js
+++ src/index.js
@@ -35,13 +35,17 @@ class Board extends React.Component { // 2 React component
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -53,7 +57,7 @@ class Board extends React.Component { // 2 React component
     };
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
